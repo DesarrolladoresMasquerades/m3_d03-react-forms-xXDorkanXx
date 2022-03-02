90b6f0361799e6e546f6d58212b343686de895cd
Fer Sánchez <sangarfer@gmail.com>
2022-03-02 13:25:17 +0100

    lunch break

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -2,22 +2,58 @@ import { useState } from "react";
 
 function AddMovie(props) {
 
+  const [formData, setFormData] = useState({
+    title: "",
+    director: "",
+    IMDBRating: "",
+    hasOscars: false
+  })
+
+  function handleDataChange(event){
+    const key = event.target.name; //key is same as inputName
+    
+    let value = "";
+    if(key === "hasOscars"){
+      value = event.target.checked;
+    }else{
+      value = event.target.value;
+    }
+
+    //setFormData(formData => Object.assign(formData, {[key]: value}));
+    setFormData(formData=> ({...formData, [key]: value}));
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    //axios.post() or fetch({method: "post"}) goes here
+
+    console.log("formData: ", formData);
+    props.addMovie(formData) // We send the sate up to the nearest parent
+
+    setFormData({
+      title: "",
+      director: "",
+      IMDBRating: "",
+      hasOscars: false
+    })
+  }
+
 
   return (
     <div className="AddMovie">
       <h4>Add a Movie</h4>
-      <form >
+      <form onSubmit={handleSubmit}>
         <label>Title:</label>
-        <input type="text" name="title" value={title} />
+        <input type="text" name="title" value={formData.title} onChange={handleDataChange}/>
 
         <label>Director:</label>
-        <input type="text" name="director" value={director}  />
+        <input type="text" name="director" value={formData.director} onChange={handleDataChange}/>
 
         <label>IMDB Rating:</label>
-        <input type="number" name="IMDBRating" value={IMDBRating}  />
+        <input type="number" name="IMDBRating" value={formData.IMDBRating} onChange={handleDataChange}/>
 
         <label>Won Oscars:</label>
-        <input type="checkbox" name="hasOscars" checked={hasOscars}  />
+        <input type="checkbox" name="hasOscars" checked={formData.hasOscars} onChange={handleDataChange}/>
         <button type="submit">Add a Movie</button>
       </form>
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,14 +6,22 @@ import AddMovie from "./AddMovie";
 import FilterMovies from "./FilterMovies";
 
 function MovieList() {
-  const [moviesData, setMoviesData] = useState(moviesDataJSON);
+  const [moviesData, setMoviesData] = useState(moviesDataJSON); //this should be your DB
   const [movies, setMovies] = useState(moviesDataJSON);
+
+  function addMovie(newMovie){
+    setMoviesData([...moviesData, newMovie])
+    // What if I want to sort or filter before setting the state?
+    setMovies([...moviesData, newMovie].sort((a, b)=>{return a.title > b.title}))
+  }
+
+  function filterMovieList
 
 
   return (
     <div>
       <FilterMovies filterMovies={filterMovieList} />
-      <AddMovie addMovie={addNewMovie} />
+      <AddMovie addMovie={addMovie} />
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
